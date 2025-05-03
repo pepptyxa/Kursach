7effc044d6e60bc4832c8f1fe1e161633e7603b8
pepptyxa <pheronerxe@gmail.com>
2025-05-03 12:19:45 +0500

    Создала класс для связи с БД. Черкашнева

--- Kursach/CLasses/Common/DBConnection.cs
+++ Kursach/CLasses/Common/DBConnection.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kursach.CLasses.Common
+namespace Kursach.Classes.Common
 {
     public class DBConnection
     {
+        public static readonly string path = "Host=localhost;Username=root;Database=kursach";
+        public static MySqlConnection Connection() 
+        {
+            MySqlConnection connection = new MySqlConnection(path);
+            connection.Open();
+            return connection;
+        }
+        public static MySqlDataReader Query(string query, MySqlConnection connection, params MySqlParameter[] parameters) 
+        {
+            var command = new MySqlCommand(query, connection);
+            command.Parameters.Add(parameters);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection) 
+        {
+            connection.Close();
+        }
     }
 }
